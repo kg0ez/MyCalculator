055b2e044bcf69602aa181e44812542b7a2012ee
kg0ez <gorboveckirill@gmail.com>
2022-10-21 23:51:14 +0300

    creat an empty project

---
+++ Calculator.App/MainWindow.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Calculator.Logic;
+using Calculator.Model;
+using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Calculator.App
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
